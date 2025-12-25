db09bb45053d6e24f790de47c95700f124258508
devyanshnandeshwar <devyansh.cloud@gmail.com>
2025-12-26 02:15:42 +0530

    feat: add user input handling for course details

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include "../include/course.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "CGPA Calculator Initialized" << endl;
+    int n;
+    vector<Course> courses;
+
+    cout << "Enter number of courses: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        Course c;
+
+        cout << "\nEnter course " << i + 1 << " details\n";
+        cout << "Course name: ";
+        cin >> c.name;
+
+        cout << "Credits: ";
+        cin >> c.credits;
+
+        cout << "Grade (S/A/B/C/D/F): ";
+        cin >> c.grade;
+        c.grade = toupper(c.grade);
+
+        courses.push_back(c);
+    }
+
+    cout << "\nCourse details entered:\n";
+    for (const auto &c : courses)
+    {
+        cout << c.name << " | Credits: " << c.credits << " | Grade: " << c.grade << endl;
+    }
+
     return 0;
 }
